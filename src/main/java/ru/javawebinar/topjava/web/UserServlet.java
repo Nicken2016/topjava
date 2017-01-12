8ae0437422306b03e3c2031afe0740896d96037a
Nicken <onicken@yandex.ru>
2017-01-13 00:23:44 +0600

    logback_HW1

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,8 +1,13 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 public class UserServlet extends javax.servlet.http.HttpServlet {
+   private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
+
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         response.sendRedirect("userList.jsp");
 //        request.getRequestDispatcher("/userList.jsp").forward(request, response);
