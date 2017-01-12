4e8b31fba24ddf0109e1495c0faf296ab2ec31fa
Nicken <onicken@yandex.ru>
2017-01-13 00:33:54 +0600

    logServlet_HW1

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -9,7 +9,9 @@ public class UserServlet extends javax.servlet.http.HttpServlet {
    private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        LOG.debug("redirect ot userList");
+
+//      request.getRequestDispatcher("/userList.jsp").forward(request, response);
         response.sendRedirect("userList.jsp");
-//        request.getRequestDispatcher("/userList.jsp").forward(request, response);
     }
 }
